867c099667e4faf8f5a815930782126ed4071589
saher <msaher.shair@gmail.com>
2025-12-25 17:09:23 +0300

    add documentation

--- lua/exo.lua
+++ lua/exo.lua
@@ -1,45 +1,50 @@
-local M = {}
+--- *exo* run commands in a throwaway buffer
+---
+--- Run external commands and show the output in a throwaway buffer
+--- Inspired by https://github.com/habamax/vim-shout
+--- By design, it does not do syntax highligting or parse error output. You can
+--- use :cgetbuffer to send the buffer's content to the quickfix list. However,
+--- it'll use https://github.com/msaher/bufix.nvim if its available
+---
+--- It internally uses vim.system(), which does NOT use a tty. If you want a
+--- tty, use |:terminal|. There are advantages to not using :terminal.
+--- 1. The buffer is modifable. You can delete or mark on comment on output.
+--- Think running commands like `:g/re/d`
+--- 2. Since there are no escape sequences, you can save the buffer as a file
+--- 3. exo buffers have a filetype. Meaning you can use custom bindings, syntax rules, etc.
 
---- @type number
-M.W_THRESHOLD = 160
+local exo = {}
 
---- @type vim.SystemObj?
-M.job = nil
+--- The width threshold before
+--- The number of columns to decide when to split vertically
+---@type number
+exo.w_threshold = 160
 
---- @type number
-M.bufnr = -1
+--- The underlying job in the exo buffer
+---@type vim.SystemObj?
+exo.job = nil
 
---- @type string
-M.time_format = "%a %b %e %H:%M:%S"
+--- The exo buffer number. Its -1 when there's no buffer
+---@type number
+exo.bufnr = -1
 
---- @type number
-M.started_at = 0
+--- Time format used in the exo buffer.
+---@type string
+exo.time_format = "%a %b %e %H:%M:%S"
 
---- @type number
-M.finished_at = 0
+--- When the job started in seconds
+---@type number
+exo.started_at = 0
+
+--- When the job finished in seconds
+---@type number
+exo.finished_at = 0
 
 local buf_set_lines = vim.schedule_wrap(vim.api.nvim_buf_set_lines)
-local notify = vim.schedule_wrap(vim.notify)
 
---- @param name string
---- @return number
-local function get_buf_by_name(name)
-    local bufs = vim.api.nvim_list_bufs()
-    for _, bufnr in ipairs(bufs) do
-        if vim.api.nvim_buf_is_loaded(bufnr) then
-            local buf_name = vim.api.nvim_buf_get_name(bufnr)
-            buf_name = vim.fn.fnamemodify(buf_name, ":t")
-            if buf_name == name then
-                return bufnr
-            end
-        end
-    end
-
-    return -1
-end
-
---- @param bufnr number
---- @return number?
+---@param bufnr number
+---@return number?
+---@private
 local function find_window_for_buf(bufnr)
     local window = nil
     for _, win in ipairs(vim.api.nvim_list_wins()) do
@@ -56,6 +61,7 @@ end
 ---@param bufnr number
 ---@param w_threshold number
 ---@return number
+---@private
 local function open_window(bufnr, w_threshold)
     local win_cfg = {}
     win_cfg.win = -1
@@ -68,27 +74,31 @@ local function open_window(bufnr, w_threshold)
     return vim.api.nvim_open_win(bufnr, false, win_cfg)
 end
 
-function M.kill()
-    if M.job ~= nil then
-        M.job:kill(15) -- SIGTERM
-        M.job:wait(150) -- wait before SIGKILL
+--- Sends SIGTERM to the job. If it does not terminate after a timeout. It
+--- sends SIGKILL
+function exo.kill()
+    if exo.job ~= nil then
+        exo.job:kill(15) -- SIGTERM
+        exo.job:wait(150) -- wait before SIGKILL
     end
 end
 
---- @param err string
---- @param data string
-function on_data(err, data)
+---@param err string
+---@param data string
+---@private
+local function on_data(err, data)
     if err ~= nil then
         vim.notify("exo: " .. err, vim.log.levels.ERROR)
     elseif data ~= nil then
-        buf_set_lines(M.bufnr, -2, -1, false, vim.split(data, "\n"))
+        buf_set_lines(exo.bufnr, -2, -1, false, vim.split(data, "\n"))
     end
 end
 
---- @param obj vim.SystemCompleted
-function on_exit(obj)
-    M.finished_at = os.time()
-    local duration_seconds = os.difftime(M.finished_at, M.started_at)
+---@param obj vim.SystemCompleted
+---@private
+local function on_exit(obj)
+    exo.finished_at = os.time()
+    local duration_seconds = os.difftime(exo.finished_at, exo.started_at)
     local duration = ""
     if duration_seconds <= 60 then
         duration = duration_seconds .. " s"
@@ -106,95 +116,89 @@ function on_exit(obj)
         status = "exited abnormally with code " .. obj.code
     end
 
-    local msg = string.format("Process %s at %s, duration %s", status, os.date(M.time_format, M.finished_at), duration)
-    buf_set_lines(M.bufnr, -2, -1, false, {"", msg})
-    M.job = nil
+    local msg = string.format("Process %s at %s, duration %s", status, os.date(exo.time_format, exo.finished_at), duration)
+    buf_set_lines(exo.bufnr, -2, -1, false, {"", msg})
+    exo.job = nil
 end
 
+--- Starts a new job in the exo buffer
 ---@param cmd string
-function M.run(cmd)
-    if M.bufnr == -1 then
-        M.bufnr = vim.api.nvim_create_buf(true, true)
-        vim.api.nvim_set_option_value('modified', false, { buf = M.bufnr})
+function exo.run(cmd)
+    if exo.bufnr == -1 then
+        exo.bufnr = vim.api.nvim_create_buf(true, true)
+        vim.api.nvim_set_option_value('modified', false, { buf = exo.bufnr})
         vim.api.nvim_create_autocmd({ "BufDelete" }, {
-            buffer = M.bufnr,
+            buffer = exo.bufnr,
             callback = function(_)
-                if M.job ~= nil then
+                if exo.job ~= nil then
                     vim.notify("exo: terminating job...")
-                    M.job:wait(100)
+                    exo.job:wait(100)
                 end
             end,
             desc = "exo: stop abandoned job",
             once = true,
         })
-        vim.api.nvim_buf_set_name(M.bufnr, "[exo]")
-        vim.api.nvim_set_option_value("filetype", "exo", { scope = "local", buf = M.bufnr })
+        vim.api.nvim_buf_set_name(exo.bufnr, "[exo]")
+        vim.api.nvim_set_option_value("filetype", "exo", { scope = "local", buf = exo.bufnr })
         local ok, bufix = pcall(require, "bufix.api")
         if ok then
-            bufix.set_buf(M.bufnr)
+            bufix.set_buf(exo.bufnr)
         end
     else
-        if M.job ~= nil then
+        if exo.job ~= nil then
             vim.notify("exo: buffer already has a job. terminating it...")
-            M.job:wait(100)
+            exo.job:wait(100)
         end
-        vim.api.nvim_buf_set_lines(M.bufnr, 0, -1, true, {})
+        vim.api.nvim_buf_set_lines(exo.bufnr, 0, -1, true, {})
     end
 
     -- window logic
-    local window = find_window_for_buf(M.bufnr)
+    local window = find_window_for_buf(exo.bufnr)
     if window == nil then
-        open_window(M.bufnr, M.W_THRESHOLD)
+        open_window(exo.bufnr, exo.w_threshold)
     end
 
-    M.started_at = os.time()
+    exo.started_at = os.time()
 
-    local modeline = "vim: filetype=exo:path+=" .. vim.fn.getcwd():gsub("^" .. vim.env.HOME, "~")
     local lines = {
         "vim: filetype=exo:path+=" .. vim.fn.getcwd():gsub("^" .. vim.env.HOME, "~"),
-        "Process started at " .. os.date(M.time_format, M.start_time),
+        "Process started at " .. os.date(exo.time_format, exo.start_time),
         "",
         cmd,
     }
-    vim.api.nvim_buf_set_lines(M.bufnr, 0, -2, true, lines)
+    vim.api.nvim_buf_set_lines(exo.bufnr, 0, -2, true, lines)
 
     local argv = { vim.o.shell, vim.o.shellcmdflag, vim.fn.escape(cmd, "\\") }
-    M.job = vim.system(argv, {
+    exo.job = vim.system(argv, {
         text = true,
         stdout = on_data,
         stderr = on_data,
     }, on_exit)
 end
 
-function M.prompt()
+--- Prompts for a command to run
+function exo.prompt()
     local opts = { prompt = "$ ", completion = "shellcmdline" }
     vim.ui.input(opts, function(input)
         if input ~= nil and input ~= "" then
-            M.run(input)
+            exo.run(input)
         end
     end)
 end
 
-function M.open()
-    if M.bufnr == -1 then
+--- Opens the exo window
+function exo.open()
+    if exo.bufnr == -1 then
         vim.notify("exo: No exo buffer to open", vim.log.levels.WARN)
         return
     end
 
-    local window = find_window_for_buf(M.bufnr)
+    local window = find_window_for_buf(exo.bufnr)
     if window == nil then
-        window = open_window(M.bufnr, M.W_THRESHOLD)
+        window = open_window(exo.bufnr, exo.w_threshold)
     end
 
     vim.api.nvim_set_current_win(window)
 end
 
-do
-    local bufnr = get_buf_by_name("[exo]")
-    if bufnr ~= -1 then
-        vim.api.nvim_buf_delete(bufnr, {force = true})
-    end
-end
-
-
-return M
+return exo
--- plugin/init.lua
+++ plugin/init.lua
@@ -1,3 +1,5 @@
+--- Runs the command in an exo buffer. It can act as drop-in replacement for *:!*
+---@tag :Exo
 vim.api.nvim_create_user_command("Exo", function(data)
     require("exo").run(data.args)
 end, {
@@ -5,11 +7,18 @@ end, {
     complete = 'shellcmdline'
 })
 
-vim.api.nvim_create_user_command("ExoPrompt", function(data)
+--- Like |:Exo|, but interactive. Prompts for a shell command to
+--- run. This is nice because you can use the <Up> and <Down>
+--- keys to move through history if you're using the default
+--- implementation of vim.ui.input
+---@tag :ExoPrompt
+vim.api.nvim_create_user_command("ExoPrompt", function(_)
     require("exo").prompt()
 end, {})
 
-vim.api.nvim_create_user_command("ExoOpen", function(data)
+--- Opens the exo window if it exists
+---@tag :ExoOpen
+vim.api.nvim_create_user_command("ExoOpen", function(_)
     require("exo").open()
 end, {})
 
